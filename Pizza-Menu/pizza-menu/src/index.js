b4d10074987393663966bc5bf222fcbff5780625
Cindy Lara <cindy123jl@gmail.com>
2024-07-02 19:50:30 -0700

    adding fragments and destructuring props

--- Pizza-Menu/pizza-menu/src/index.js
+++ Pizza-Menu/pizza-menu/src/index.js
@@ -80,11 +80,17 @@ function Menu() {
       <h2>Our Menu</h2>
 
       {numPizzas > 0 ? (
-        <ul className="pizzas">
-          {pizzas.map((pizza) => (
-            <Pizza pizzaObj={pizza} key={pizza.name} />
-          ))}
-        </ul>
+        <>
+          <p>
+            Authentic Italian cuisine. 6 creative dishes to choose from. All
+            from our stone oven, all organic, all delicious.
+          </p>
+          <ul className="pizzas">
+            {pizzas.map((pizza) => (
+              <Pizza pizzaObj={pizza} key={pizza.name} />
+            ))}
+          </ul>
+        </>
       ) : (
         <p>We are still working on our menu. Please come back later :)</p>
       )}
@@ -106,18 +112,18 @@ function Menu() {
   );
 }
 
-function Pizza(props) {
-  // console.log(props);
+function Pizza({ pizzaObj }) {
+  console.log(pizzaObj);
 
-  if (props.pizzaObj.soldOut) return null;
+  if (pizzaObj.soldOut) return null;
 
   return (
     <li className="pizza">
-      <img src={props.pizzaObj.photoName} alt={props.pizzaObj.name}></img>
+      <img src={pizzaObj.photoName} alt={pizzaObj.name}></img>
       <div>
-        <h3>{props.pizzaObj.name}</h3>
-        <p>{props.pizzaObj.ingredients}</p>
-        <span>{props.pizzaObj.price + 3}</span>
+        <h3>{pizzaObj.name}</h3>
+        <p>{pizzaObj.ingredients}</p>
+        <span>{pizzaObj.price + 3}</span>
       </div>
     </li>
   );
@@ -141,12 +147,7 @@ function Footer() {
   return (
     <footer className="footer">
       {isOpen ? (
-        <div className="order">
-          <p>
-            We are open until {closeHour}:00. Come visit us or order online.
-          </p>
-          <button className="btn">Order</button>
-        </div>
+        <Order closeHour={closeHour} openHour={openHour} />
       ) : (
         <p>
           We are currently closed. Come back tomorrow between {openHour}
@@ -158,6 +159,16 @@ function Footer() {
 
   // old way of doing it
   //return React.createElement("footer", null, "We're Currently Open");
+}
+
+function Order({ closeHour, openHour }) {
+  <div className="order">
+    <p>
+      We are open from {openHour}:00 to {closeHour}:00. Come visit us or order
+      online.
+    </p>
+    <button className="btn">Order</button>
+  </div>;
 }
 
 //this is how we render in the root
